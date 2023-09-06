a18ebac9e87bc8132a4886444ada836764643134
hum-projects <118135689+hum-projects@users.noreply.github.com>
2023-09-06 15:57:03 +0100

    Add files via upload

---
+++ spin.py
@@ -0,0 +1,51 @@
+import PyQt5.QtWidgets as qtw
+import PyQt5.QtGui as qtg
+
+class MainWindow(qtw.QWidget):
+	def __init__(self):
+		super().__init__()
+		# Add a tittle
+		self.setWindowTitle("Hello World!!!")
+
+		# Set Vertical layout
+		self.setLayout(qtw.QVBoxLayout())
+
+		# Create a label
+		my_label = qtw.QLabel("Pick Something From The List Below")
+		# Change the font size of label
+		my_label.setFont(qtg.QFont('Helvetica', 24))
+		self.layout().addWidget(my_label)
+
+		# Create an spin box
+		my_spin = qtw.QDoubleSpinBox(self,
+			value=10,
+			maximum=100,
+			minimum=0,
+			singleStep=5,
+			prefix="#",
+			suffix="!!!"
+			)
+		# Change font size of spinbox
+		my_spin.setFont(qtg.QFont('Helvetica', 18))
+
+		# Put combobox on the screen
+		self.layout().addWidget(my_spin)
+
+		# Create a button
+		my_button = qtw.QPushButton("Press me", 
+			clicked = lambda: press_it())
+		self.layout().addWidget(my_button)
+
+		# Show the app
+		self.show()
+
+		def press_it():
+			# Add name to label
+			my_label.setText(f'You Picked {my_spin.value()}!')
+			
+
+app = qtw.QApplication([])
+mw = MainWindow()
+
+# Run The App
+app.exec_()
